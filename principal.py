8ec0354cd5b3f1ed86c631864fb29c3b2e844374
carinealencar <carinealencar@outlook.com>
2025-12-13 19:20:39 -0300

    Update principal.py

--- principal.py
+++ principal.py
@@ -2,18 +2,18 @@ import streamlit as st
 import os
 
 st.set_page_config(
-    page_title="Dashboard das notas do Enem nos últimos 5 anos",
+    page_title="Dashboard das notas do Enem nos últimos anos",
     page_icon="📊",
     layout="centered",
     initial_sidebar_state="expanded"
 )
 
-st.title('Dashboard das notas do Enem nos últimos 5 anos 📊')
+st.title('Dashboard das notas do Enem nos últimos anos 📊')
 
 
 st.header('Filtros:')
 ano = st.selectbox('Escolha o ano para a análise:',
-    ('2020', '2021', '2022', '2023', '2024'))
+    ('2020', '2021', '2022', '2023'))
 
 filtro = st.selectbox('Escolha uma variável para análise:',
                                     sorted(['Renda', 'Ano de conclusão', 'Raça']))
