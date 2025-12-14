5d47f976d8fc29adb460dc5c72c5cf5e28ddb4f3
carinealencar <carinealencar@outlook.com>
2025-12-13 23:45:01 -0300

    Update principal.py

--- principal.py
+++ principal.py
@@ -151,26 +151,28 @@ if botao:
         )
     
         st.plotly_chart(fig_sexo, use_container_width=True)
-    with col2:
+   with col2:
         st.markdown("### 📊 Média Geral das Notas por Faixa Etária")
-
+    
         df_faixa = df[
             (df['TP_PRESENCA_CH'] == 1) & (df['TP_PRESENCA_CN'] == 1) & (df['TP_PRESENCA_MT'] == 1) & (df['TP_PRESENCA_LC'] == 1) & (df['TP_FAIXA_ETARIA'].notna())
         ].copy()
         df_faixa['Faixa Etária'] = df_faixa['TP_FAIXA_ETARIA'].map(m_faixa_etaria)
+        df_faixa = df_faixa.dropna(subset=['Faixa Etária'])
         df_faixa['MEDIA_GERAL'] = df_faixa[['NU_NOTA_CH', 'NU_NOTA_CN', 'NU_NOTA_MT', 'NU_NOTA_LC']].mean(axis=1)
-        df_media_faixa = (df_faixa.groupby('TP_FAIXA_ETARIA')['MEDIA_GERAL'].mean().reset_index().sort_values('TP_FAIXA_ETARIA'))
-    
+        df_media_faixa = (df_faixa.groupby('Faixa Etária')['MEDIA_GERAL'].mean().reset_index())
+        df_media_faixa = df_media_faixa.sort_values('Faixa Etária')
+        
         fig_faixa = px.bar(
             df_media_faixa,
             x='MEDIA_GERAL',
-            y='TP_FAIXA_ETARIA',
+            y='Faixa Etária', 
             orientation='h',
             labels={
-                'TP_FAIXA_ETARIA': 'Faixa Etária',
+                'Faixa Etária': 'Faixa Etária',
                 'MEDIA_GERAL': 'Média Geral das Notas'
             }
         )
-    
+        
         st.plotly_chart(fig_faixa, use_container_width=True)
-
+    
